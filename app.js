0d16b3ff8293a9b9c3babfaece3af807dbe3c8e4
Niels <74902241+NielsHak@users.noreply.github.com>
2021-04-07 10:24:59 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const hamburgerLines = document.querySelectorAll('.line');
 const navLinks = document.querySelector('.nav-links');
 
 toggle.addEventListener('click', () => {
+    navLinks.classList.toggle('hide')
     toggle.classList.toggle('active')
     hero.classList.toggle('active')
     hamburgerLines.forEach((line) => {
         line.classList.toggle('black')
     })
-    navLinks.classList.toggle('hide')
 });
